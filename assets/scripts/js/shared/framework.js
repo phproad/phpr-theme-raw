b16bb09129a02c8ebb995b3649ab18df4a7ca7b5
Sam Georges <sam@daftspunk.com>
2013-03-20 19:48:01 +1100

    Init

---
+++ assets/scripts/js/shared/framework.js
@@ -0,0 +1,55 @@
+/**
+ * Loads framework items
+ */
+
+/*
+
+// Bootstrap
+@require ../../../extras/foundationstrap/bootstrap/js/bootstrap-transition.js;
+@require ../../../extras/foundationstrap/bootstrap/js/bootstrap-alert.js;
+@require ../../../extras/foundationstrap/bootstrap/js/bootstrap-button.js;
+@require ../../../extras/foundationstrap/bootstrap/js/bootstrap-carousel.js;
+@require ../../../extras/foundationstrap/bootstrap/js/bootstrap-collapse.js;
+@require ../../../extras/foundationstrap/bootstrap/js/bootstrap-dropdown.js;
+@require ../../../extras/foundationstrap/bootstrap/js/bootstrap-modal.js;
+@require ../../../extras/foundationstrap/bootstrap/js/bootstrap-tooltip.js;
+@require ../../../extras/foundationstrap/bootstrap/js/bootstrap-popover.js;
+@require ../../../extras/foundationstrap/bootstrap/js/bootstrap-scrollspy.js;
+@require ../../../extras/foundationstrap/bootstrap/js/bootstrap-tab.js;
+@require ../../../extras/foundationstrap/bootstrap/js/bootstrap-typeahead.js;
+@require ../../../extras/foundationstrap/bootstrap/js/bootstrap-affix.js;
+@require ../../../extras/foundationstrap/bootstrap/js/bootstrap-modal.js;
+@require ../../../extras/foundationstrap/bootstrap/js/bootstrap-tooltip.js;
+@require ../../../extras/foundationstrap/bootstrap/js/bootstrap-popover.js;
+@require ../../../extras/foundationstrap/bootstrap/js/bootstrap-scrollspy.js;
+
+// Utility
+@require ../../../extras/utility/js/jquery.ui.widget.js;
+@require ../../../extras/utility/js/jquery.gmap3.js;
+@require ../../../extras/utility/js/jquery.fileupload.js;
+@require ../../../extras/utility/js/jquery.waterfall.js;
+@require ../../../extras/utility/js/jquery.maskedinput.js;
+@require ../../../extras/utility/js/jquery.typewatch.js;
+@require ../../../extras/utility/js/jquery.utility.dropdown.js;
+@require ../../../extras/utility/js/jquery.utility.toggletext.js;
+@require ../../../extras/utility/js/jquery.utility.gmap.js;
+@require ../../../extras/utility/js/jquery.utility.gmap_locator.js;
+@require ../../../extras/utility/js/jquery.utility.popup.js;
+@require ../../../extras/utility/js/jquery.utility.stars.js;
+@require ../../../extras/utility/js/jquery.utility.statusbar.js;
+@require ../../../extras/utility/js/jquery.utility.scrollbar.js;
+@require ../../../extras/utility/js/jquery.utility.uploader.js;
+@require ../../../extras/utility/js/utility.js;
+
+// Ahoy
+@require ../../../extras/ahoy/js/jquery.validate.js;
+@require ../../../extras/ahoy/js/jquery.validate.ext.js;
+@require ../../../extras/ahoy/js/ahoy.post.js;
+@require ../../../extras/ahoy/js/ahoy.form.js;
+@require ../../../extras/ahoy/js/ahoy.validation.js;
+@require ../../../extras/ahoy/js/ahoy.js;
+
+// App
+@require common.js
+
+*/
